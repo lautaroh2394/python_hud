c6e7b517792b12580a4859b48f2c3ea58b3fa776
lautaroh2394 <lautarohaase_23@hotmail.com>
2021-05-11 00:01:36 -0300

    ahora se puede definir el color del texto correspondiente a una key al actualizar el widget texto con un json

--- HUD.py
+++ HUD.py
@@ -6,10 +6,14 @@ class HUD():
     execution_thread = None
     raiz = None
     label_texto = None
+    text_widget = None
 
     @staticmethod
-    def create_widgets():
+    def create_widgets(opts={}):
+        #Opts es un dict con una key por cada elemento del hud, cada key es un booleano. Tiene que estar específicamente en True para mostrar dicho elemento
+
         HUD.raiz = Tk()
+        HUD.raiz.title("Python HUD para Nico")
         HUD.raiz.geometry("300x300")
         HUD.raiz.protocol("WM_DELETE_WINDOW", HUD.close_callback)
         #HUD.raiz.protocol("WM_DELETE_WINDOW", HUD.raiz.quit)
@@ -18,21 +22,39 @@ class HUD():
         frame.pack(side="top")
         frame.config(width=300, height=300, bg="red")
 
-        label = Label(frame, text="prueba")
-        label.pack()
+        if "label" in opts and opts["label"]:
+            label = Label(frame, text="prueba")
+            #label.pack(side="left")
+            label.pack()
 
-        HUD.label_texto = StringVar()
-        label['textvariable'] = HUD.label_texto
-        HUD.label_texto.set("No hay datos")
+            HUD.label_texto = StringVar()
+            label['textvariable'] = HUD.label_texto
+            HUD.label_texto.set("No hay datos")
+        
+        if "text" in opts and opts["text"]:
+            HUD.text_widget = Text(frame, width = 40, height = 300)
+            HUD.text_widget.pack()
+            HUD.text_widget['state'] = 'normal'
+            HUD.text_widget.delete(0.0, HUD.text_widget.index('end'))
+            HUD.text_widget.insert('0.0',"No hay datos")
+            HUD.text_widget['state'] = 'disabled'
+            
+            '''
+            HUD.text_widget.tag_add('prueba', '1.0', '1.6')
+            HUD.text_widget.tag_add('prueba2', '2.3', '2.6')
+            HUD.text_widget.tag_configure('prueba', background='yellow', font='TkFixedFont', relief='raised')
+            HUD.text_widget.tag_configure('prueba2', background='red', font='TkFixedFont', relief='raised')
+            #HUD.text_widget['state'] = 'disabled'
+            '''
 
     @staticmethod
-    def thread_start():
-        HUD.execution_thread = threading.Thread(target=HUD.start)
+    def thread_start(opts = {"text": True}):
+        HUD.execution_thread = threading.Thread(target=HUD.start,args=[opts])
         HUD.execution_thread.start()    
 
     @staticmethod
-    def start():
-        HUD.create_widgets()
+    def start(opts):
+        HUD.create_widgets(opts)
         HUD.raiz.mainloop()
 
     @staticmethod
@@ -43,7 +65,7 @@ class HUD():
 
     @staticmethod
     def update_label(texto):
-        if HUD.label_texto is not None and HUD.execution_thread.is_alive():
+        if HUD.exists(HUD.label_texto):
             if type(texto) is str:
                 HUD.label_texto.set(texto)
                 return
@@ -51,6 +73,36 @@ class HUD():
                 t = "\n".join([f"{key}: {texto[key]}" for key in texto.keys()])
                 HUD.label_texto.set(t)
     
+    @staticmethod
+    def update_text(texto):
+        if HUD.exists(HUD.text_widget):
+            #Borro lo anterior
+            HUD.text_widget['state'] = 'normal'
+            HUD.text_widget.delete(0.0, HUD.text_widget.index('end'))
+            
+            if type(texto) is str:
+                HUD.text_widget.insert("0.0", texto)
+                return
+            if type(texto) is dict:
+                #TODO: Aceptar dicts con arrays y subdicts
+                indice = 0
+                for key in texto.keys():
+                    if "_color" not in key:
+                        color_key = f"{key}_color"
+                        linea = f"{key}: {texto[key]}\n"
+                        prox_indice = indice + linea.count("\n")
+                        HUD.text_widget.insert('end',linea)
+                        if color_key in texto:
+                            HUD.text_widget.tag_add(color_key, f"{indice+1}.0", f"{prox_indice + 1}.0")
+                            HUD.text_widget.tag_configure(color_key, background=texto[color_key], font='TkFixedFont', relief='raised')
+                        indice = prox_indice
+
+            HUD.text_widget['state'] = 'disabled'
+
+    @staticmethod
+    def exists(elem):
+        return elem is not None and HUD.execution_thread.is_alive()
+
     @staticmethod
     def close_callback():
         print("close callback")
--- app.py
+++ app.py
@@ -3,18 +3,38 @@ import time
 from HUD import HUD
 HUD.thread_start()
 
-while 1:
-    #Actualizo la hora cada 0.1 seg    
-    t = time.localtime()
-    #time_formated = f"{t.tm_year}/{t.tm_mon}/{t.tm_mday} - {t.tm_hour}:{t.tm_min}:{t.tm_sec}"
-    time_formated = {
-        "año": t.tm_year,
-        "mes": t.tm_mon,
-        "dia": t.tm_mday,
-        "hora": t.tm_hour,
-        "min": t.tm_min,
-        "sec": t.tm_sec
-    }
-    print(time_formated)
-    HUD.update_label(time_formated)
-    time.sleep(1)
+def run_app():
+    while HUD.execution_thread.is_alive():
+        #Actualizo la hora cada 0.1 seg    
+        t = time.localtime()
+        #time_formated = f"{t.tm_year}/{t.tm_mon}/{t.tm_mday} - {t.tm_hour}:{t.tm_min}:{t.tm_sec}"
+        time_formated = {
+            "año": t.tm_year,
+            "mes": t.tm_mon,
+            "dia": t.tm_mday,
+            "hora": t.tm_hour,
+            "min": t.tm_min,
+            "sec": t.tm_sec
+        }
+        print(time_formated)
+        HUD.update_label(time_formated)
+
+        #Actualizo el texto:
+        #Para darle color a una linea debo agregar la key f"{key_origina}_color"
+        time_formated = {
+            "año": t.tm_year,
+            "mes": t.tm_mon,
+            "dia": t.tm_mday,
+            "hora": t.tm_hour,
+            "min": t.tm_min,
+            "sec": t.tm_sec,
+            "año_color": "green",
+            "mes_color": "blue",
+            "dia_color": "yellow",
+            "hora_color": "red"
+        }
+        HUD.update_text(time_formated)
+        time.sleep(1)
+
+if __name__ == "__main__":
+    run_app()
